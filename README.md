ae43a61473ffc2d8abf518960e419eccc414631b
Zigecek <72701569+Zigecek@users.noreply.github.com>
2021-04-14 22:29:15 +0200

    file upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# node-sec2human
-Node package that converts seconds to 00:00:00 format.
+## Function
+* Example response:
+    ```JS
+    const sec2human = require('sec2human');
+
+    console.log(sec2human(8941));
+    // 02:29:01
+
+    console.log(sec2human('41'));
+    // 00:41
+
+    console.log(sec2human(92));
+    // 01:32
+
+    console.log(sec2human(0));
+    // undefined
+
+    console.log(sec2human(true));
+    // undefined
+
+    console.log(sec2human('xx'));
+    // undefined
+    ```
+## Installation
+* `npm i sec2human --save`
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Converts seconds to 00:00:00 format.
+ * @param {(Number|String)} seconds Seconds to convert.
+ * @returns {String} String in 00:00:00 format.
+ */
+function sec2human(seconds) {
+    if (Number(seconds) != 0 && !isNaN(Number(seconds)) && seconds != undefined && seconds != null && (typeof seconds == "string" || typeof seconds == "number")) {
+        seconds = Number(seconds);
+        const pad = n => n.toString().padStart(2, '0');
+        const h = pad(Math.floor(+seconds / 3600));
+        const m = pad(Math.floor(+seconds % 3600 / 60));
+        const s = pad(Math.floor(+seconds % 3600 % 60));
+        var resp = (h == 00 && m == 00) ? ('00:' + s) : (h == 00 && m != 00) ? (m + ':' + s) : (h + ':' + m + ':' + s);
+        return resp;
+    } else {
+        return undefined;
+    }
+};
+module.exports = sec2human;
